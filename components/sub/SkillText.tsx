c3bd61d8baed689fe9d9335c609b6ea3ccda2368
Dhmhtrhs07 <128792447+Dhmhtrhs07@users.noreply.github.com>
2024-09-11 12:13:48 +0300

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -20,7 +20,7 @@ const SkillText = () => {
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
         >
-            Bulding Sites with modern technologies
+            Building Sites with modern technologies
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
